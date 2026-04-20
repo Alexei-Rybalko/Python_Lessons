fbe46a0f57bdd8b1230691b990b62738ddc7dcf5
Alexei-Rybalko <agenxalexei@gmail.com>
2026-04-20 23:22:35 +0300

    Решил задачу про олимпиаду

---
+++ Pobediteli_Olimpiadi/Olimpiada.py
@@ -0,0 +1,37 @@
+#Переменные
+uchastniki = input("Напишите количество учинеков: ")
+balls = input("Напишите количество правильных ответов у учеников через запятую: ")
+balls1 = balls.split(", ")
+winner = 0
+debil_est = False
+#Проверка на выход за рамки, а также поиск количества ответов у победителя
+for pobeda in balls1:
+    if int(pobeda) >= 20 and int(pobeda) != 20:
+        print("EROR")
+        winner = 0
+        break
+    if int(pobeda) <= 1 and int(pobeda) != 1:
+        print("EROR")
+        winner = 0
+        break
+    if int(uchastniki) <= 1 and int(uchastniki) != 1:
+        print("EROR")
+        winner = 0
+        break
+    if int(uchastniki) >= 50 and int(uchastniki) != 50:
+        print("EROR")
+        winner = 0
+        break
+    if winner <= int(pobeda) and int(pobeda) != winner:
+        winner = 0
+        winner += int(pobeda)
+#Проверка есть ли человек который ничего не решил
+for debil in balls1:
+    if int(debil) == 0:
+        debil_est = True
+#Вывод результатов
+if winner != 0:
+    print(winner)
+if winner != 0:
+    print(debil_est)
+        
